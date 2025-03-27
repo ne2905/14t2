89213b1b195e63dfdcde7a7275454b8e2bfaee85
ne2905 <newibu2905@gmail.com>
2025-03-27 15:09:50 +0700

    Bai2.cpp

--- 1/Bai2.cpp
+++ 1/Bai2.cpp
@@ -1,25 +1,50 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
-void chuyen(int n, char A, char B, char C) {
-	if (n == 1) {
-		cout << "chuyen 1 dia tu "<< A <<"sang " << C << endl;
-	}
-	else{ 
-	chuyen(n-1, A , C , B);
-	cout <<"chuyen dia "<< n << "tu " << A << "sang " << C << endl;
-	chuyen(n-1, B, A, C);
-	}
+struct DoVat {
+    int giaTri, khoiLuong;
+    double tyLe;
+};
+
+bool cmp(DoVat a, DoVat b) {
+    return a.tyLe > b.tyLe;
 }
-int main(){
-	int n;
-	cout<<"nhap so luong dia: "<< endl;
-	cin >>n;
-	if (n<1) {
-		cout <<" so dia khong hop le"<<endl;
-		return 1;
-	}
-	cout <<"quy trinh chuyen dia: "<<endl;
-	chuyen(n ,'A' , 'B' , 'C');
-	return 0;
+
+void xepBaLo(vector<DoVat> &danhSach, int w) {
+    sort(danhSach.begin(), danhSach.end(), cmp);
+
+    double tongGiaTri = 0;
+    int trongLuongConLai = w;
+
+    cout << "Các vật được chọn:" << endl;
+    for (auto &doVat : danhSach) {
+        if (trongLuongConLai >= doVat.khoiLuong) {
+            cout << "Giá trị: " << doVat.giaTri << ", Khối lượng: " << doVat.khoiLuong << endl;
+            tongGiaTri += doVat.giaTri;
+            trongLuongConLai -= doVat.khoiLuong;
+        }
+    }
+
+    cout << "Tổng giá trị đạt được: " << tongGiaTri << endl;
+}
+
+int main() {
+    int n, w;
+    cout << "Nhập số lượng đồ vật: ";
+    cin >> n;
+    cout << "Nhập trọng lượng tối đa của ba lô: ";
+    cin >> w;
+
+    vector<DoVat> danhSach(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Nhập giá trị và khối lượng của đồ vật " << i + 1 << ": ";
+        cin >> danhSach[i].giaTri >> danhSach[i].khoiLuong;
+        danhSach[i].tyLe = (double)danhSach[i].giaTri / danhSach[i].khoiLuong;
+    }
+
+    xepBaLo(danhSach, w);
+    return 0;
 }
