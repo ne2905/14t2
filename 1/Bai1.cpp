9abb0cfc26300d23756788461d7566051df06132
ne2905 <newibu2905@gmail.com>
2025-03-27 15:08:27 +0700

    Bai1.cpp

--- 1/Bai1.cpp
+++ 1/Bai1.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
 using namespace std;
-int fibonacci (int n) {
-	if (n ==1 || n == 2) 
-	return 1;
-		return fibonacci(n - 1) + fibonacci( n- 2);
+
+void doiTien(int m) {
+    int menhGia[] = {10000, 5000, 2000, 1000, 500};
+    int soTo[5] = {0};
+
+    for (int i = 0; i < 5; i++) {
+        soTo[i] = m / menhGia[i];
+        m %= menhGia[i];
+    }
+
+    if (m > 0) {
+        cout << "Không thể đổi đủ số tiền." << endl;
+    } else {
+        cout << "Số tờ tiền cần dùng:" << endl;
+        for (int i = 0; i < 5; i++) {
+            if (soTo[i] > 0) {
+                cout << menhGia[i] << "đ: " << soTo[i] << " tờ" << endl;
+            }
+        }
+    }
 }
 
 int main() {
-	int n;
-	cout <<"Nhap so nguyen duong n: ";
-	cin >> n;
-	if (n <= 0 ) {
-	cout <<" vui long nhap so nguyen duong " << endl;
-	return 1;
-	}
-	cout << "So fibonacci thu "<< n << " la: " <<fibonacci(n) << endl;
-	cout << "Day fibonacci tu 1 den " << n << ": ";
-	for (int i=1; i<=n; i++){
-		cout<< fibonacci(i) << " ";
-	}
-	cout << endl;
-	return 0;
-	}
-	
-
+    int m;
+    cout << "Nhập số tiền cần đổi: ";
+    cin >> m;
+    doiTien(m);
+    return 0;
+}
