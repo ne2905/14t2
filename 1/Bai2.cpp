36e570d05f63339734ed8123f8eef60ef50e6802
ne2905 <newibu2905@gmail.com>
2025-04-10 15:29:11 +0700

    Update Bai2.cpp

--- 1/Bai2.cpp
+++ 1/Bai2.cpp
@@ -1,50 +1,39 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-struct DoVat {
-    int giaTri, khoiLuong;
-    double tyLe;
-};
-
-bool cmp(DoVat a, DoVat b) {
-    return a.tyLe > b.tyLe;
+// Hàm in mảng
+void printArray2(int x[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << x[i] << " ";
+    }
+    cout << endl;
 }
 
-void xepBaLo(vector<DoVat> &danhSach, int w) {
-    sort(danhSach.begin(), danhSach.end(), cmp);
-
-    double tongGiaTri = 0;
-    int trongLuongConLai = w;
-
-    cout << "Các vật được chọn:" << endl;
-    for (auto &doVat : danhSach) {
-        if (trongLuongConLai >= doVat.khoiLuong) {
-            cout << "Giá trị: " << doVat.giaTri << ", Khối lượng: " << doVat.khoiLuong << endl;
-            tongGiaTri += doVat.giaTri;
-            trongLuongConLai -= doVat.khoiLuong;
+// Hàm Bubble Sort (sắp xếp giảm dần)
+void bubbleSort2(int x[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = n - 1; j > i; j--) {
+            if (x[j] > x[j - 1]) { // Sắp xếp giảm dần
+                int tg = x[j];
+                x[j] = x[j - 1];
+                x[j - 1] = tg;
+            }
         }
     }
-
-    cout << "Tổng giá trị đạt được: " << tongGiaTri << endl;
 }
 
 int main() {
-    int n, w;
-    cout << "Nhập số lượng đồ vật: ";
-    cin >> n;
-    cout << "Nhập trọng lượng tối đa của ba lô: ";
-    cin >> w;
-
-    vector<DoVat> danhSach(n);
-    for (int i = 0; i < n; i++) {
-        cout << "Nhập giá trị và khối lượng của đồ vật " << i + 1 << ": ";
-        cin >> danhSach[i].giaTri >> danhSach[i].khoiLuong;
-        danhSach[i].tyLe = (double)danhSach[i].giaTri / danhSach[i].khoiLuong;
-    }
-
-    xepBaLo(danhSach, w);
+    cout << "Bai 2: Sap xep lua chon voi Bubble Sort\n";
+    int x[] = {50, 8, 34, 6, 98, 17, 83, 25, 66, 42, 21, 59, 63, 71, 85};
+    int n = sizeof(x) / sizeof(x[0]);
+    
+    cout << "Day so ban dau: ";
+    printArray2(x, n);
+    
+    bubbleSort2(x, n);
+    
+    cout << "Day so sau khi sap xep giam dan: ";
+    printArray2(x, n);
+    
     return 0;
 }
