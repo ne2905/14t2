a780c1876e4df8c73a7e00a72ef943c1fce03d45
ne2905 <newibu2905@gmail.com>
2025-04-24 15:56:41 +0700

    Update Bai2.cpp

--- 1/Bai2.cpp
+++ 1/Bai2.cpp
@@ -1,27 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Thuật toán tìm kiếm nhị phân đệ quy
 int binary_search(int x[], int left, int right, int k) {
     if (left > right)
-        return -1;
-    int mid = (left + right) / 2;
-    if (x[mid] == k)
-        return mid;
-    else if (x[mid] > k)
-        return binary_search(x, left, mid - 1, k);
-    else
-        return binary_search(x, mid + 1, right, k);
+        return -1; // Dãy rỗng, tìm kiếm không thành công
+    else {
+        int mid = (left + right) / 2;
+        if (x[mid] == k) return mid;
+        else if (x[mid] > k)
+            return binary_search(x, left, mid - 1, k);
+        else
+            return binary_search(x, mid + 1, right, k);
+    }
 }
 
-void bai2() {
+int main() {
     int x[] = {4, 14, 24, 34, 54, 64, 74, 84, 94};
     int n = sizeof(x) / sizeof(x[0]);
 
-    int k1 = 34, k2 = 60;
-    int pos1 = binary_search(x, 0, n - 1, k1);
-    int pos2 = binary_search(x, 0, n - 1, k2);
+    int k;
+    cout << "Nhap so can tim: ";
+    cin >> k;
 
-    cout << "\nBai 2:\n";
-    (pos1 != -1) ? cout << "Tim thay " << k1 << " tai vi tri " << pos1 << endl
-                 : cout << "Khong tim thay " << k1 << endl;
+    int result = binary_search(x, 0, n - 1, k);
+    if (result != -1)
+        cout << "Tim thay " << k << " o vi tri thu " << result << " trong mang." << endl;
+    else
+        cout << "Khong tim thay " << k << " trong mang." << endl;
 
-    (pos2 != -1) ? cout << "Tim thay " << k2 << " tai vi tri " << pos2 << endl
-                 : cout << "Khong tim thay " << k2 << endl;
+    return 0;
 }
