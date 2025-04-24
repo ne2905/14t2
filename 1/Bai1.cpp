86a9ccfe821cffc33c46569472a69b528ebcc774
ne2905 <newibu2905@gmail.com>
2025-04-24 15:28:35 +0700

    Update Bai1.cpp

--- 1/Bai1.cpp
+++ 1/Bai1.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int search(int x[], int n, int k) {
+int linearSearch(int x[], int n, int k) {
     int i = 0;
-    while (i < n && x[i] != k)
+    while (i < n && x[i] != k) {
+        cout << "i = " << i << "; x[" << i << "] = " << x[i]
+             << " != " << k << " => i = i + 1" << endl;
         i++;
-    return (i < n) ? i : -1;
+    }
+
+    if (i < n) {
+        cout << "i = " << i << "; x[" << i << "] = " << k << " => return i" << endl;
+        return i;
+    } else {
+        cout << "i = " << i << " >= n => return -1" << endl;
+        return -1;
+    }
 }
 
-void bai1() {
+int main() {
     int x[] = {34, 14, 24, 54, 84, 64, 94, 74, 4};
     int n = sizeof(x) / sizeof(x[0]);
 
-    int k1 = 94, k2 = 55;
-    int pos1 = search(x, n, k1);
-    int pos2 = search(x, n, k2);
+    int k;
+    cout << "Nhap so can tim: ";
+    cin >> k;
 
-    cout << "Bai 1:\n";
-    (pos1 != -1) ? cout << "Tim thay " << k1 << " tai vi tri " << pos1 << endl
-                 : cout << "Khong tim thay " << k1 << endl;
+    int result = linearSearch(x, n, k);
 
-    (pos2 != -1) ? cout << "Tim thay " << k2 << " tai vi tri " << pos2 << endl
-                 : cout << "Khong tim thay " << k2 << endl;
+    if (result != -1)
+        cout << "Tim thay " << k << " tai vi tri " << result << " trong day." << endl;
+    else
+        cout << "Khong tim thay " << k << " trong day." << endl;
+
+    return 0;
 }
