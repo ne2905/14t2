f1ef3db8469bae81101e966add06d56d1626a00e
ne2905 <newibu2905@gmail.com>
2025-10-11 11:01:25 +0700

    Update Bai3.cpp

--- 1/Bai3.cpp
+++ 1/Bai3.cpp
@@ -1,35 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="vi">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bài Tập JavaScript Cơ Bản</title>
+    <title>Bài 7: Tam giác bằng while</title>
     <style>
-        .chao-ong {
-            text-decoration: underline; 
-        }
-        .chao-ban {
-            color: red; 
+        pre {
+            font-size: 18px;
+            line-height: 1.2;
         }
     </style>
 </head>
 <body>
-    <h1>Bài 3: Chia các số từ 1 đến 100</h1>
-    <div id="greetings"></div>
+    <h2>Bài 7: Sử dụng while để in tam giác</h2>
+    <pre id="tamgiacWhile"></pre>
+
     <script>
-        let greetingsDiv = document.getElementById("greetings");
-        
-        for (let i = 1; i <= 100; i++) {
-            if (i % 3 === 0 && i % 5 === 0) {
-                greetingsDiv.innerHTML += "<span class='chao-ban'>chao ban " + i + "</span><br>";
-            } 
-            else if (i % 3 === 0) {
-                greetingsDiv.innerHTML += "<span class='chao-ong'>chao ong " + i + "</span><br>";
-            } 
-            else if (i % 5 === 0) {
-                greetingsDiv.innerHTML += "chao ba " + i + "<br>";
+        let tamgiacWhile = "";
+        let i = 1;
+        while (i <= 4) {
+            let j = 1;
+            while (j <= i) {
+                tamgiacWhile += "*";
+                j++;
             }
+            tamgiacWhile += "\n";
+            i++;
         }
+        document.getElementById("tamgiacWhile").textContent = tamgiacWhile;
     </script>
 </body>
 </html>
