7760da24e3a4d6b6115570b02cd62042b2392ba3
ne2905 <newibu2905@gmail.com>
2026-04-22 15:57:02 +0700

    Update Bai2.cpp

--- 1/Bai2.cpp
+++ 1/Bai2.cpp
@@ -1,16 +1,5 @@
-body{font-family:Arial;margin:0;padding:0}
-header{background:#000;color:#fff;padding:10px;text-align:center;}
-#header-logo{display:flex;justify-content:space-between;align-items:center}
-#header-logo img{height:50px}
-#menu{background:#008000;color:#000;display:flex;gap:20px;padding:10px;font-size:16px;font-weight:bold;}
-#menu a{text-decoration:none;color:#000;}
-#menu a:hover{color:#EAFB6F;}
-.submenu{display:none;position:absolute;background:#ccc;padding:10px;}
-.menu-item:hover .submenu{display:block;background:#008000;}
-#layout{display:grid;grid-template-columns:200px 1fr 200px;grid-template-rows:auto 1fr auto;height:100vh;}
-#sidebar1,#sidebar2{background:#eee;padding:10px;}
-#content{padding:10px;}
-footer{background:rgb(0,0,255);color:#fff;text-align:center;padding:10px;font-size:14px;}
-form{border:1px solid #000;width:300px;padding:10px;margin-top:20px;}
-input{width:100%;margin:5px 0;}
-button{margin-top:10px;}
+public override void Output()
+{
+    Console.Write("{0,-10} {1,-20} {2,-10} {3,-15} {4,-10} {5,-15} {6,-15}",
+        id, fullName, gender, specialty, experience, mobile, workingHours);
+}
