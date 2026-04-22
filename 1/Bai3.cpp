fde9046b5aa069ff990f9980877c5d85feafdc79
ne2905 <newibu2905@gmail.com>
2026-04-22 16:12:56 +0700

    Remove HTML content and add Display function

--- 1/Bai3.cpp
+++ 1/Bai3.cpp
@@ -1,94 +1,21 @@
-<!DOCTYPE html>
-<html lang="vi">
-<head>
-    <meta charset="UTF-8">
-    <title>Khoa CNTT - Đại học Bách Khoa Hà Nội</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="container">
-        <!-- HEADER -->
-        <header>
-            <img src="logo-left.png" alt="Logo Bách Khoa" class="logo-left">
-            <div class="header-text">
-                <h2>TRƯỜNG ĐẠI HỌC BÁCH KHOA HÀ NỘI</h2>
-                <h3>KHOA CÔNG NGHỆ THÔNG TIN</h3>
-            </div>
-            <img src="logo-right.png" alt="Logo SOICT" class="logo-right">
-        </header>
+static void Display(List<GeneralPractitioner> list)
+{
+    if (list.Count == 0)
+    {
+        Console.WriteLine("Danh sach rong!");
+        return;
+    }
 
-        <div class="main">
-            <!-- MENU TRÁI -->
-            <nav class="sidebar">
-                <ul class="menu">
-                    <li><a href="#">Trang chủ</a></li>
-                    <li><a href="#">Các khoa</a>
-                        <ul class="submenu">
-                            <li><a href="#">Khoa CNTT</a></li>
-                            <li><a href="#">Khoa Điện</a></li>
-                            <li><a href="#">Khoa Hóa</a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Hỗ trợ sinh viên</a></li>
-                    <li><a href="#">Đăng ký học phần</a></li>
-                    <li><a href="#">Câu lạc bộ Tin</a></li>
-                    <li><a href="#">Kết bạn bốn phương</a></li>
-                    <li><a href="#">Đào tạo LTV</a></li>
-                </ul>
-            </nav>
+    Console.WriteLine("\n---------------------------------------------------------------------------------------------------------------");
+    Console.WriteLine("{0,-10} {1,-20} {2,-10} {3,-15} {4,-10} {5,-15} {6,-15}",
+        "ID", "Ten", "GioiTinh", "ChuyenKhoa", "KinhNghiem", "SDT", "GioLam");
+    Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
 
-            <!-- NỘI DUNG CHÍNH -->
-            <section class="content">
-                <div class="news">
-                    <img src="student1.png" alt="Sinh viên 1" class="avatar">
-                    <p>Tốt nghiệp xuất sắc với điểm trung bình tích lũy toàn khóa 3.93/4, nam sinh trường <b>Hà Tĩnh</b> Lai Ngọc Thắng 
-                    Long trở thành thủ khoa đầu ra khóa 63 của Đại học Bách khoa Hà Nội...</p>
-                </div>
-                <div class="news">
-                    <img src="student2.png" alt="Sinh viên 2" class="avatar">
-                    <p>Tốt nghiệp xuất sắc với điểm trung bình tích lũy toàn khóa 3.94/4, nữ sinh trường <b>Hà Tĩnh</b> Lai Ngọc Thắng 
-                    Long trở thành thủ khoa đầu ra khóa 63 của Đại học Bách khoa Hà Nội...</p>
-                </div>
-                <div class="news">
-                    <img src="student3.png" alt="Sinh viên 3" class="avatar">
-                    <p>Tốt nghiệp xuất sắc với điểm trung bình tích lũy toàn khóa 3.92/4, nam sinh trường <b>Hà Tĩnh</b> Lai Ngọc Thắng 
-                    Long trở thành thủ khoa đầu ra khóa 63 của Đại học Bách khoa Hà Nội...</p>
-                </div>
-            </section>
+    foreach (var x in list)
+    {
+        x.Output();
+        Console.WriteLine();
+    }
 
-            <!-- THANH PHẢI -->
-            <aside class="right">
-                <div class="login">
-                    <h4>Đăng ký / Đăng nhập</h4>
-                </div>
-                <div class="support">
-                    <h4>Hỗ trợ sinh viên</h4>
-                    <p>Gọi ngay book tour hỗ trợ:<br>
-                    <b>Hotline:</b> 0948844000<br>
-                    <b>MS:</b> 158708<br>
-                    <b>Ms. Hương</b></p>
-                </div>
-                <div class="search">
-                    <h4>TÌM KIẾM</h4>
-                    <form>
-                        <label>Họ tên:</label><br>
-                        <input type="text"><br>
-                        <label>Mã SV:</label><br>
-                        <input type="text"><br>
-                        <label>Giới tính:</label><br>
-                        <input type="text"><br>
-                        <label>Địa chỉ:</label><br>
-                        <input type="text"><br>
-                        <button>Tìm kiếm</button>
-                    </form>
-                </div>
-            </aside>
-        </div>
-
-        <!-- FOOTER -->
-        <footer>
-            <p>Bản quyền khoa CNTT - Đại học Bách khoa Hà Nội</p>
-        </footer>
-    </div>
-</body>
-</html>
+    Console.WriteLine("---------------------------------------------------------------------------------------------------------------");
+}
